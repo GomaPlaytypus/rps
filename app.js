6439b32db8c68ab4e146ac03569ef4ce0d168451
Goma <gomaplatypus@gmail.com>
2022-12-29 12:02:00 -0800

    Got rid of the switch function and changed randomChoice to Math.floor(Math.random() * rpsChoices.length);

--- app.js
+++ app.js
@@ -3,15 +3,53 @@ const rpsChoices = [
     'paper',
     'scissors'
 ];
+const playerSelection = "".toLowerCase();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice(randomChoice, rpsChoices) {
-    var randomChoice = Math.floor(Math.random(rpsChoices) * 2);
+    var randomChoice = Math.floor(Math.random() * rpsChoices.length);
+
 };
 
-function playRound(playerSelection, ComputerSelection) {
-
+switch (playerSelection) {
+        case "rock":
+        console.log("You chose Rock");
+        break;
+        case "paper":
+        console.log("You chose Paper");
+        case "scissors":
+        console.log("You chose Scissors");
+        break;
 }
 
-const playerSelection = "";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == 0) {
+        console.log("It's a tie!");
+    } else if (playerSelection == "paper" && computerSelection == 0) {
+        playerScore++
+        console.log("Paper beats rock! You win!");
+    } else if (playerSelection == "scissors" && computerSelection == 0) {
+        compScore++
+        console.log("Rock beats scissors! You Lose!");
+    } else if (playerSelection == "rock" && computerSelection == 1) {
+        compScore++
+        console.log("Paper beats rock! You lose!");
+    } else if (playerSelection == "paper" && computerSelection == 1) {
+        console.log("It's a tie!");
+    } else if (playerSelection == "scissors" && computerSelection == 1) {
+        playerScore++
+        console.log("Scissors beats paper! You win!");
+    } else if (playerSelection == "rock" && computerSelection == 2) {
+        playerScore++
+        console.log("Rock beats scissors! You win!");
+    } else if (playerSelection == "paper" && computerSelection == 2) {
+        compScore++
+        console.log("Scissors beats paper! You lose!");
+    } else if (playerSelection == "scissors" && computerSelection == 2) {
+        console.log("It's a tie!");
+    }
+};
+
+const game = () => {
+
+}
