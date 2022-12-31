c7c6e39b2cac3eb1a253342bd1e5818140390c52
Goma <gomaplatypus@gmail.com>
2022-12-31 10:24:18 -0800

    Installed Jest (committed so the popup would go away)

--- app.js
+++ app.js
@@ -1,55 +1,49 @@
-const rpsChoices = [
-    'rock',
-    'paper',
-    'scissors'
-];
-const playerSelection = "".toLowerCase();
-const computerSelection = getComputerChoice();
-
-function getComputerChoice(randomChoice, rpsChoices) {
-    var randomChoice = Math.floor(Math.random() * rpsChoices.length);
-
-};
-
-switch (playerSelection) {
-        case "rock":
-        console.log("You chose Rock");
-        break;
-        case "paper":
-        console.log("You chose Paper");
-        case "scissors":
-        console.log("You chose Scissors");
-        break;
+const getComputerChoice = () => {
+    const rpsChoices = [
+        'rock', 'paper', 'scissors']    
+    const randomChoice = Math.floor(Math.random() * rpsChoices.length);
+    return rpsChoices[randomChoice]
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == 0) {
-        console.log("It's a tie!");
+        console.log("It's a tie! You both chose rock!");
     } else if (playerSelection == "paper" && computerSelection == 0) {
         playerScore++
-        console.log("Paper beats rock! You win!");
+        console.log("You win! Paper beats rock!");
     } else if (playerSelection == "scissors" && computerSelection == 0) {
         compScore++
-        console.log("Rock beats scissors! You Lose!");
+        console.log("You Lose! Rock beats scissors!");
     } else if (playerSelection == "rock" && computerSelection == 1) {
         compScore++
-        console.log("Paper beats rock! You lose!");
+        console.log("You lose! Paper beats rock!");
     } else if (playerSelection == "paper" && computerSelection == 1) {
-        console.log("It's a tie!");
+        console.log("You both chose paper! It's a tie!");
     } else if (playerSelection == "scissors" && computerSelection == 1) {
         playerScore++
-        console.log("Scissors beats paper! You win!");
+        console.log("You win! Scissors beats paper!");
     } else if (playerSelection == "rock" && computerSelection == 2) {
         playerScore++
-        console.log("Rock beats scissors! You win!");
+        console.log("You win! Rock beats scissors!");
     } else if (playerSelection == "paper" && computerSelection == 2) {
         compScore++
-        console.log("Scissors beats paper! You lose!");
+        console.log("You lose! Scissors beats paper! ");
     } else if (playerSelection == "scissors" && computerSelection == 2) {
-        console.log("It's a tie!");
+        console.log("You both chose scissors! It's a tie!");
     }
-};
+}
 
 const game = () => {
-
+for (let i = 0; i < 5; i++) {
+const playerSelection = prompt('Choose your move!', 'Rock, Paper, Scissors'.toLowerCase())
+const computerSelection = getComputerChoice();
+console.log('1 ', playRound(playerSelection, computerSelection));
+}
+if (playerScore > compScore) {
+    console.log("You beat the computer! Good Job!")
+} else if (playerScore < compScore) {
+    console.log("You lose! Try again!")
+} else {
+    console.log("...Wow... You tied!")
+}
 }
